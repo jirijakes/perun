94e00553aa3eafd4a19087f8d43e6c5b538414f8
jiri <>
2021-05-26 22:19:33 +0000

    Introduce yet unused custom crypto types
    FossilOrigin-Name: 6e647bb7586dcd9e49a19157d90428afc886b366378ebd0976d954b386390151

--- src/main/scala/crypto.scala
+++ src/main/scala/crypto.scala
@@ -1,4 +1,39 @@
 package perun.crypto
 
+import scodec.bits.ByteVector
+import scala.annotation.targetName
+
 enum DecryptionError:
   case BadTag
+
+opaque type PublicKey = ByteVector
+opaque type PrivateKey = (ByteVector, PublicKey)
+opaque type Signature = ByteVector
+opaque type DoubleSHA256 = ByteVector
+
+object PublicKey:
+  def fromBytes(b: ByteVector): PublicKey = b
+
+object PrivateKey:
+  def fromHex(h: String): PrivateKey = ??? //ByteVector.fromValidHex(h)
+
+extension (k: PublicKey)
+  @targetName("publicKeyToArray")
+  def toArray: Array[Byte] = k.toArray
+  @targetName("publicKeyLength")
+  def length: Int = k.length.toInt
+  @targetName("publicKeyToBytes")
+  def bytes: ByteVector = k
+
+extension (k: PrivateKey)
+  @targetName("privateKeyToArray")
+  def toArray: Array[Byte] = k._1.toArray
+  def publicKey: PublicKey = k._2
+
+extension (s: Signature)
+  @targetName("signatureToArray")
+  def toArray: Array[Byte] = s.toArray
+
+extension (d: DoubleSHA256)
+  @targetName("doubleSHA256ToArray")
+  def toArray: Array[Byte] = d.toArray
