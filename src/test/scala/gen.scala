39651111c2a32ec20221c9a1250a74c71c33ed79
jiri <>
2021-05-31 11:34:57 +0000

    Prepare Gen
    FossilOrigin-Name: c05d01f9f6697e11b4abb4d6cde4973389196113fd25c40b08647eb16eea3163

--- src/test/scala/gen.scala
+++ src/test/scala/gen.scala
@@ -4,20 +4,30 @@ import zio.*
 import zio.random.*
 import zio.test.*
 
+import perun.crypto.keygen.*
+import perun.proto.blockchain.Chain
 import perun.proto.codecs.*
 import perun.proto.gossip.ChannelAnnouncement
-import perun.crypto.keygen.*
+
+val anyChain: Gen[Random, Chain] =
+  Gen.elements(Chain.Testnet, Chain.Mainnet, Chain.Signet)
 
 val validKeyPair: Gen[Has[Keygen], PrivateKey] =
   Gen.fromEffect(generateKeypair)
 
 val validSignature: Gen[Any, Signature] = ???
 
+val validShortChannelId: Gen[Random, ShortChannelId] =
+  Gen.zipN(Gen.int(0, 25000), Gen.int(0, 100), Gen.int(0, 10))(
+    ShortChannelId.apply
+  )
+
 val validChannelAnnouncement: Gen[Has[Keygen] & Random, ChannelAnnouncement] =
   (validKeyPair <*> validKeyPair <*> validKeyPair <*> validKeyPair)
     .flatMap { case (((node1, node2), btc1), btc2) =>
       for
-        i <- Gen.alphaChar
+        chain <- anyChain
+        shortChannelId <- validShortChannelId
         nodeId1 = node1.publicKey
         nodeId2 = node2.publicKey
         bitcoinKey1 = btc1.publicKey
