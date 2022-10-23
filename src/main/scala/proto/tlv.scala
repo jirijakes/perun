595f1aeceb1c0a3dc27879a748831241e84f0be7
jiri <>
2022-10-23 11:41:47 +0000

    Add TLV documentation
    FossilOrigin-Name: 5d88b43d30f7829364d95da09a6504aed798aa9d3de76086f316924d41c07156

--- src/main/scala/proto/tlv.scala
+++ src/main/scala/proto/tlv.scala
@@ -7,60 +7,71 @@ import scodec.{Attempt, Codec, Err}
 import perun.proto.codecs.*
 import perun.proto.uint64.*
 
+/** Single field of TLV stream.
+  *
+  * @param tag type of record
+  * @param content raw (encoded) value
+  */
+final case class Record(tag: Long, content: ByteVector)
+
+/** Series of raw (encoded) TLV records. */
 opaque type TlvStream = Map[Long, ByteVector]
 
-extension (s: TlvStream) def find(tag: Long): Option[ByteVector] = s.get(tag)
+extension (s: TlvStream)
+  /** Obtain raw (encoded) record of type `tag`.
+    *
+    * @param tag type of TLV record
+    * @return raw (encoded) record if found, otherwise `None`
+    */
+  def find(tag: Long): Option[ByteVector] = s.get(tag)
+
+  /** Add value and its type to the stream.
+    *
+    * @param tag type of record
+    * @param value value
+    * @return updated TLV stream
+    */
+  def put(tag: Long, value: ByteVector): TlvStream = s.updated(tag, value)
 
 object TlvStream:
   def apply(m: Map[Long, ByteVector]): TlvStream = m
   def empty: TlvStream = Map.empty
 
-type Tlv[T <: Tuple] <: Tuple = T match
-  case (Long, Codec[h])      => Option[h] *: EmptyTuple
-  case (Long, Codec[h]) *: t => Option[h] *: Tlv[t]
-  case EmptyTuple            => EmptyTuple
+/** Match type that converts TLV `type -> codec` pairs into options
+  * of codec types. For example, `((Long, Codec[Int]), (Long, Codec[String]))`
+  * will become `(Option[Int], Option[String])`.
+  */
+type Tlv[Mapping <: Tuple] <: Tuple = Mapping match
+  case (Long, Codec[t]) *: rest => Option[t] *: Tlv[rest]
+  case (Long, Codec[t])         => Option[t] *: EmptyTuple
+  case EmptyTuple               => EmptyTuple
 
-def tlv[T <: Tuple](t: T): Codec[Tlv[T]] =
-  tlvStream.exmap(mapToTlv(t), tlvToMap(t))
-
-private def mapToTlv[T <: Tuple](t: T)(m: TlvStream): Attempt[Tlv[T]] =
-  t.toIArray
-    .map { case (i: Long, c: Codec[?]) =>
-      m.find(i)
-        .fold(Attempt.successful(Option.empty))(b =>
-          c.decodeValue(b.toBitVector).map(Option(_))
-        )
-    }
-    .foldLeft(Attempt.successful(IArray.emptyObjectIArray))((acc, cur) =>
-      acc.flatMap(arr => cur.map(c => arr :+ c))
-    )
-    .map(arr => runtime.Tuples.fromIArray(arr).asInstanceOf[Tlv[T]])
-
-private def tlvToMap[T <: Tuple](t: T)(tt: Tlv[T]): Attempt[TlvStream] =
-  t.zip(tt)
-    .toIArray
-    .map { case ((a: Long, b: Codec[h]), c: Option[?]) =>
-      b.encode(c.asInstanceOf[Option[h]].get).map(b => (a, b))
-    }
-    .foldLeft(Attempt.successful(Map.empty[Long, ByteVector]))((acc, cur) =>
-      acc.flatMap(macc => cur.map((i, b) => macc.updated(i, b.toByteVector)))
+extension [A](codec: Codec[A])
+  /** Validate value encoded by this codec using provided function.
+    * Decoding is not modified in any way.
+    *
+    * @param validate validating function
+    * @return derived codec that validates encoded value
+    */
+  def validateEncoding(validate: A => Either[String, A]): Codec[A] =
+    codec.exmap(
+      a =>
+        validate(a) match
+          case Left(error)  => Attempt.Failure(Err(error))
+          case Right(valid) => Attempt.Successful(valid)
+      ,
+      Attempt.successful
     )
 
-final case class Record(tag: Long, content: ByteVector)
-
+/** Codec for one encoded TLV record. */
 val record: Codec[Record] =
-  (bigsize :: variableSizeBytesLong(bigsize, bytes)).as[Record]
+  ("type" | bigsize ::
+    variableSizeBytesLong("length" | bigsize, "value" | bytes)).as[Record]
 
-// format: off
-extension [A](c: Codec[A])
-  def validateEncoding[E](f: A => Either[String, A]): Codec[A] =
-    c.exmap(a => f(a) match {
-      case Left(s) => Attempt.Failure(Err(s))
-      case Right(x) => Attempt.Successful(x)
-    }, Attempt.successful
-  )
-// format: on
-
+/** Codec for encoded TLV stream (`tlv_stream` type). This codec does not decode values,
+  * for decoded values (that is what is typically needed when implementing BOLT protocol),
+  * see [[tlv]].
+  */
 val tlvStream: Codec[TlvStream] =
   vector(record)
     .validateEncoding(v =>
@@ -77,3 +88,56 @@ val tlvStream: Codec[TlvStream] =
       _.map(r => (r.tag, r.content)).toMap,
       _.toVector.map((t, c) => Record(t, c)).sortBy(_.tag)
     )
+
+/** Codec for decoded TLV stream. Values are decoded using the provided `mapping`, where each
+  * possible type is described in form `type -> codec`. Type of decoded value is a tuple
+  * with the same number of members as there are types and their types corresponding,
+  * in order, to types of provided codecs.
+  *
+  * @example {{{
+  * val myCodec: Codec[(Option[Int], Option[String])] =
+  *   tlv(0L -> uint16, 1L -> utf8)
+  * }}}
+  *
+  * @param mapping mapping of types to codec
+  * @return new codec
+  */
+inline def tlv[Mapping <: Tuple](mapping: Mapping): Codec[Tlv[Mapping]] =
+  tlvStream.exmap(runtime.encodeStream(mapping), runtime.decodeStream(mapping))
+
+private object runtime:
+
+  def encodeStream[Mapping <: Tuple](
+      mapping: Mapping
+  )(stream: TlvStream): Attempt[Tlv[Mapping]] =
+    mapping.toIArray
+      .map { case (tag: Long, codec: Codec[?]) =>
+        stream
+          .find(tag)
+          .fold(Attempt.successful(Option.empty))(bytes =>
+            codec.decodeValue(bytes.toBitVector).map(Option(_))
+          )
+      }
+      .foldLeft(Attempt.successful(IArray.emptyObjectIArray))((acc, cur) =>
+        acc.flatMap(arr => cur.map(c => arr :+ c))
+      )
+      .map(arr =>
+        scala.runtime.Tuples.fromIArray(arr).asInstanceOf[Tlv[Mapping]]
+      )
+
+  def decodeStream[Mapping <: Tuple](
+      mapping: Mapping
+  )(tlv: Tlv[Mapping]): Attempt[TlvStream] =
+    mapping
+      .zip(tlv)
+      .toIArray
+      .map { case ((tag: Long, codec: Codec[t]), value: Option[?]) =>
+        codec
+          .encode(value.asInstanceOf[Option[t]].get)
+          .map(bits => (tag, bits.toByteVector))
+      }
+      .foldLeft(Attempt.successful(TlvStream.empty))((acc, cur) =>
+        acc.flatMap(macc => cur.map(macc.put))
+      )
+
+end runtime
